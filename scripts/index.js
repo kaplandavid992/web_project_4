9da669c97230a6c70c03cf2570ca1bbb1756db0a
kaplandavid992 <kaplandavid992@gmail.com>
2022-01-04 00:24:10 +0200

    feat: six card initial

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,46 @@ let nameInput = document.querySelector(".popup__form-input_field_name");
 let roleInput = document.querySelector(".popup__form-input_field_role");
 let profileName = document.querySelector(".profile__name");
 let profileRole = document.querySelector(".profile__role");
+let cardTemplate = document.querySelector("#card-template");
+let cardsList = document.querySelector(".elements__list");
 
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+function createCard(card) {
+  let clonedCard = cardTemplate.content.querySelector(".elements__element").cloneNode(true);
+  clonedCard.querySelector(".elements__image").src = card.link;
+  clonedCard.querySelector(".elements__text").textContent = card.name;
+  return clonedCard;
+}
+
+function insertCard(card) {
+  cardsList.appendChild(card);
+}
 
 function togglePopupDisplay() {
   popup.classList.toggle("popup_active");
@@ -33,3 +72,6 @@ profileEditBtn.addEventListener("click", editProfile);
 popupExitBtn.addEventListener("click", exitForm);
 popupForm.addEventListener("submit", saveForm);
 
+initialCards.forEach((card) => {
+ insertCard(createCard(card));
+})
