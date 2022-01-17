63b1e4e7e3e84a5d8e0213ba5893c7ad97bcf1da
kaplandavid992 <kaplandavid992@gmail.com>
2022-01-17 21:22:33 +0200

    wip input eeror msg style

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,9 +1,8 @@
-
 //If any field doesn't pass validation, the "Save" button should be inactive.
 
- //If both fields pass validation, then they should be active.
- 
- //Use the colors from the design for the inactive buttons.
+//If both fields pass validation, then they should be active.
+
+//Use the colors from the design for the inactive buttons.
 
 // enabling validation by calling enableValidation()
 // pass all the settings on call
@@ -16,47 +15,44 @@
 //     errorClass: "popup__error_visible"
 //   });
 
-const popupForm = document.querySelector(".popup__form");
-const popupFormInput = popupForm.querySelector(".popup__form-input");
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__form-errorMsg_active");
+};
 
-  const showInputError = (formElement, inputElement , errorMessage) => {
-    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add("popup__form-errorMsg_active");
-  };
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  errorElement.classList.remove("popup__form-errorMsg_active");
+  // errorElement.textContent = "";
+};
 
-  const hideInputError = (formElement, inputElement) => {
-    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    errorElement.classList.remove("popup__form-errorMsg_active");
-    errorElement.textContent = "";
-  };
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
 
-
-
-  const checkInputValidity = (formElement, inputElement) => {
-    if (!inputElement.validity.valid) {
-      showInputError(formElement, inputElement, inputElement.validationMessage);
-    } else {
-      hideInputError(formElement, inputElement);
-    }
-  };
-  
-  popupForm.addEventListener("submit", function (evt) {
-    evt.preventDefault();
-  });
-  
-  popupFormInput.addEventListener("input", function () {
-    checkInputValidity(popupForm, popupFormInput);
-  });
-
-
-  function setEventListeners(formElement) {
-    const inputList = Array.from(formElement.querySelectorAll(".popup__form-input"));
-    inputList.forEach( inputElement => {
-        console.log(inputElement);
-      inputElement.addEventListener("input", function () {
-          checkInputValidity(formElement, inputElement);
-      })
+function setEventListeners(formElement) {
+  const inputList = Array.from(
+    formElement.querySelectorAll(".popup__form-input")
+  );
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
     });
-  };
-    setEventListeners(popupForm);
+  });
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+}
+enableValidation();
